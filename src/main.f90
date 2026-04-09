b780b5b654b71f81c419322e542617d30c1054fb
Chris D'Aleo <129748984+ChrisD4430@users.noreply.github.com>
2026-04-09 12:41:08 -0400

    update main.f90

--- src/main.f90
+++ src/main.f90
@@ -11,22 +11,28 @@ program heat_diffusion_calculator
   integer :: ierr, nsteps, step
   real(kind=8) :: t
 
+  ! Load simulation settings from config file
   call read_config(cfg, 'config/heat_config.in', ierr)
   if (ierr /= 0) stop "Configuration error"
 
+  ! Build the grid (positions along the rod)
   call init_grid(g, cfg%x_min, cfg%x_max, cfg%n_points)
   allocate(temp(g%n_points))
 
+  ! Create the initial temperature distribution
   call initialize_temperature(g, temp, cfg%temp_left, cfg%temp_right)
 
+  ! Determine how many update steps to run
   nsteps = int(cfg%t_final / cfg%dt)
   t = 0.0d0
 
+  ! Main simulation loop
   do step = 1, nsteps
      call step_diffusion(g, temp, cfg%diffusivity, cfg%dt)
      t = t + cfg%dt
   end do
 
+  ! Save the final temperature profile
   call write_temperature_csv('output/final_temperature.csv', g, temp)
 
   deallocate(temp)

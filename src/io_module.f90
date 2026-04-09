6be620d13675136ca6e0f8b367c04a6beabb116c
Chris D'Aleo <129748984+ChrisD4430@users.noreply.github.com>
2026-04-09 12:40:25 -0400

    simplified output to a CSV file

--- src/io_module.f90
+++ src/io_module.f90
@@ -6,6 +6,7 @@ module io_module
 
 contains
 
+  ! Writes the final temperature distribution to a CSV file
   subroutine write_temperature_csv(filename, g, temp)
     character(len=*), intent(in) :: filename
     type(line_grid),  intent(in) :: g
@@ -14,9 +15,14 @@ contains
     integer :: unit, i
 
     unit = 20
+
+    ! Create or overwrite the output file
     open(unit=unit, file=filename, status='replace', action='write')
 
+    ! Header row for spreadsheet/plotting tools
     write(unit, '(A)') 'position,temperature'
+
+    ! Write each position and its temperature
     do i = 1, g%n_points
        write(unit, '(F15.8,1x,F15.8)') g%x(i), temp(i)
     end do

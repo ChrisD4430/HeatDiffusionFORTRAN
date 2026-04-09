39e7163c739d59407bc6c577c29f217abc8b0fb1
Chris D'Aleo <129748984+ChrisD4430@users.noreply.github.com>
2026-04-09 12:35:55 -0400

    updated heat grid logic

--- src/grid_module.f90
+++ src/grid_module.f90
@@ -3,14 +3,16 @@ module grid_module
   private
   public :: line_grid, init_grid
 
+  ! Represents the rod as a set of evenly spaced positions
   type :: line_grid
-     real(kind=8), allocatable :: x(:)
-     real(kind=8)              :: dx
+     real(kind=8), allocatable :: x(:)   ! Positions along the rod
+     real(kind=8)              :: dx     ! Spacing between points
      integer                   :: n_points
   end type line_grid
 
 contains
 
+  ! Initializes the grid based on user settings
   subroutine init_grid(g, x_min, x_max, n_points)
     type(line_grid), intent(out) :: g
     real(kind=8),    intent(in)  :: x_min, x_max
@@ -20,8 +22,11 @@ contains
 
     g%n_points = n_points
     allocate(g%x(n_points))
+
+    ! Compute spacing between points
     g%dx = (x_max - x_min) / real(n_points - 1, kind=8)
 
+    ! Fill the position array
     do i = 1, n_points
        g%x(i) = x_min + real(i - 1, kind=8) * g%dx
     end do
